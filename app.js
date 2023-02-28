02e3587cb1d48f6a6483e564163e1de1a0bf761c
Avish-Dilaksha <adilaksha@gmail.com>
2023-02-28 11:43:43 +0530

    Created connect.js
    Created connect.js and connected to mongoDB data base

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
+const connectDB = require('./db/connect')
+
+app.use(express.json())
+
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, console.log(`server is listening on ${PORT}`))
+const start = async () => {
+    try {
+        // connect to DB
+        mongoose.set("strictQuery", false);
+        await connectDB(process.env.MONGO_URI)
+        app.listen(PORT, console.log(`server is listening on ${PORT}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
