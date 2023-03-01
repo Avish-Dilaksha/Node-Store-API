e2706eae56cc89e1ef76f24bcb655772e8f5e90a
Avish-Dilaksha <adilaksha@gmail.com>
2023-03-01 08:48:43 +0530

    Created asyncWrapper.js
    created asyncWrapper.js to wrap up all the crud functions so we don't need to iplement try catch in each function

--- controllers/product.js
+++ controllers/product.js
@@ -1,34 +1,35 @@
 const Product = require('../models/product')
+const asyncwrapper = require('../middleware/async')
  
-const getAllProducts = async (req, res) => {
+const getAllProducts = asyncwrapper(async (req, res) => {
     const products = await Product.find({})
     res.status(200).json({products})
- }
+ })
 
- const getProduct = async (req, res) => {
+ const getProduct = asyncwrapper( async (req, res) => {
     const {id:productID} = req.params
     const product = await Product.findOne({_id: productID})
     if(!product) {
-        return new Error(`No product with id: ${productID}`, 404)
+        return new Error(`No product with id: ${productID}`)
     }
     res.status(200).json({product})
- }
+ })
 
- const createProduct = async (req, res) => {
+ const createProduct = asyncwrapper( async (req, res) => {
     const product = await Product.create(req.body)
     res.status(200).json({product})
- }
+ })
 
- const deleteProduct = async (req, res) => {
+ const deleteProduct = asyncwrapper( async (req, res) => {
     const {id:productID} = req.params
     const product = await Product.findOneAndDelete({_id:productID})
     if(!product) {
         return new Error(`No product with id: ${productID}`, 404)
     }
     res.status(200).json({task: null, status: 'success'})
- }
+ })
 
- const updateProduct = async (req, res) => {
+ const updateProduct = asyncwrapper( async (req, res) => {
     const {id:productID} = req.params
     const product = await Product.findOneAndUpdate({_id: productID}, req.body, {
         new : true,
@@ -38,7 +39,7 @@ const getAllProducts = async (req, res) => {
         return new Error(`No product with id: ${productID}`, 404)
     }
     res.status(200).json({product})
- }
+ })
 
  module.exports = {
     getAllProducts,
